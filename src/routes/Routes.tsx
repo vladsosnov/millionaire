c9dd07d3ced1692b450b3ac13fb9787ad62fea3a
vlad-sosnov-jll <Vladyslav.Sosnov@am.jll.com>
2022-12-28 15:39:17 +0100

    MR-9 feat: add routes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Main, Game, Result } from 'src/pages';
+import { routes } from './appRoutes';
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/game" element={<Game />} />
-      <Route path="/result" element={<Result />} />
+      <Route path={routes.main.path} element={<Main />} />
+      <Route path={routes.game.path} element={<Game />} />
+      <Route path={routes.result.path} element={<Result />} />
       <Route path="*" element={<div>Page Not Found</div>} />
     </Routes>
   );
