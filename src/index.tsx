30fcdc880f8308788ea62470c0dcb6de306c9242
vlad-sosnov-jll <Vladyslav.Sosnov@am.jll.com>
2022-12-28 14:16:34 +0100

    MR-6 feat: add reset and root variables

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import { App } from './App';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { BrowserRouter } from 'react-router-dom';
+import 'src/assets/styles/reset.css';
+import 'src/assets/styles/shared.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
